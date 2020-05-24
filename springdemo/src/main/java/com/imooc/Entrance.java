64e1a6d1bfecafdcc5e752c1d9442bd17947e2b5
liangbingtian <liangbtd@yonyou.com>
2020-05-24 21:54:41 +0800

    学习spring

---
+++ springdemo/src/main/java/com/imooc/Entrance.java
@@ -0,0 +1,12 @@
+package com.imooc;
+
+/**
+ * Created by liangbingtian on 2020/5/24 9:35 下午
+ */
+public class Entrance {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
